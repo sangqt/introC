14ea4687e67339a2fae6aeb6526aae6351ab632f
Shou <sangqt.inb@gmail.com>
2024-06-11 16:44:21 +0900

    some small updates

--- hello.c
+++ hello.c
@@ -3,5 +3,11 @@
 int main(int argc, char *argv[])
 {
     printf("Hello, World!\n");
+    int x = 1;
+    int *pX = &x;
+    int **ppX = &pX;
+    int **pppX = &ppX;
+
+    printf("%p\n", *pppX);
     return 0;
 }
